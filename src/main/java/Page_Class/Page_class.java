00729df096b0adb021a9017f614807ff063238b9
Deepak <deepakkhetre26@gmail.com>
2024-01-15 07:35:50 +0530

    First Commit

---
+++ src/main/java/Page_Class/Page_class.java
@@ -0,0 +1,12 @@
+package Page_Class;
+
+public class Page_class {
+
+    public void logintopage(){
+        System.out.println("Login done");
+    }
+    public void logout(){
+        System.out.println("Logout done");
+    }
+
+}
